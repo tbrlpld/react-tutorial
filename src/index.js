a39c4a371f3dbe1cb21a52fdbdb6f6749bbafca1
Tibor Leupold <tibor.leupold@torchbox.com>
2021-12-22 14:10:04 -0800

    Log square clicks

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 
 function Square(props) {
     return (
-      <button className="square">
-          { props.value }
-      </button>
+        <button className="square" onClick={() => {console.log('click')} }>
+            { props.value }
+        </button>
     );
 }
 
