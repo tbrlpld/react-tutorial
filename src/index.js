01179eb115a48464191b585d165200633f542318
Tibor Leupold <tibor.leupold@torchbox.com>
2021-12-22 14:02:49 -0800

    Convert board to function component

--- src/index.js
+++ src/index.js
@@ -11,35 +11,33 @@ function Square(props) {
     );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
+function Board() {
+    function renderSquare(i) {
+        return <Square value={i} />;
+    }
 
-  render() {
     const status = 'Next player: X';
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     );
-  }
 }
 
 class Game extends React.Component {
