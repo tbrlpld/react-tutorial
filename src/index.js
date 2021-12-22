a8469a40837e10093b056e6fd5f47ebc86520a69
Tibor Leupold <tibor.leupold@torchbox.com>
2021-12-22 14:00:06 -0800

    Convert square to function component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
       <button className="square">
-          { this.props.value }
+          { props.value }
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
