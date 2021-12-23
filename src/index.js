f7019f985505d6e68a1a4ac39053082ec6e524f0
Tibor Leupold <tibor.leupold@torchbox.com>
2021-12-23 14:41:41 -0800

    Compute winner from fields (once per render)

--- src/index.js
+++ src/index.js
@@ -11,10 +11,37 @@ function Square(props) {
     );
 }
 
+function computeWinner (squares) {
+    const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let line of winningLines) {
+        const [a, b, c] = line
+        if (
+            squares[a] != null
+            && squares[a] === squares[b]
+            && squares[a] === squares[c]
+        ){
+            return squares[a]
+        }
+    }
+    return null
+}
+
 function Board() {
     const [fields, setFields] = useState(Array(9).fill(null))
     const [xIsNext, setXIsNext] = useState(true)
-    const [winner, setWinner] = useState(null)
+
+    const winner = computeWinner(fields)
 
     function getPlayerLabel() {
         return xIsNext ? "X" : "O"
